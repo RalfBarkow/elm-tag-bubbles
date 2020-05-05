57a4ed2892fcd2de264571565885e5c64f51a364
Ralf Barkow <ralf.barkow@me.com>
2020-05-05 12:18:15 +0200

    "elm-version": "0.19.1"

--- src/Main.elm
+++ src/Main.elm
@@ -1,24 +1,23 @@
-import UI
-import Init
+module Main exposing (..)
 
 import Browser
 import Browser.Events
+import Init
+import UI
 
 
 main =
-  Browser.element
-    { init = Init.init
-    , update = UI.update
-    , subscriptions = subscriptions
-    , view = UI.view
-    }
+    Browser.element
+        { init = Init.init
+        , update = UI.update
+        , subscriptions = subscriptions
+        , view = UI.view
+        }
 
 
 subscriptions : UI.Model -> Sub UI.Msg
 subscriptions model =
-  Sub.batch
-    [ Browser.Events.onAnimationFrame UI.Tick
-    , Browser.Events.onResize UI.Resize
-    ]
-
-
+    Sub.batch
+        [ Browser.Events.onAnimationFrame UI.Tick
+        , Browser.Events.onResize UI.Resize
+        ]
